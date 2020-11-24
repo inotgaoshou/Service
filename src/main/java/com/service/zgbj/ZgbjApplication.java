e2b8c2b4d4a034236d2d1f9886ff20c67c3c2102
chenyubin <496172717@qq.com>
2020-11-24 14:19:38 +0800

    增加日志输出

--- src/main/java/com/service/zgbj/ZgbjApplication.java
+++ src/main/java/com/service/zgbj/ZgbjApplication.java
@@ -6,6 +6,9 @@ import com.corundumstudio.socketio.SocketConfig;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
 import com.service.zgbj.mysqlTab.DataBaseService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -22,17 +25,21 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.net.InetAddress;
 
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
 @SpringBootApplication
-//@ServletComponentScan(basePackages = "com.service.zgbj.servlet")
+//@Slf4j
 public class ZgbjApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ZgbjApplication.class);
 
     public static void main(String[] args) {
 
         try {
             SpringApplication.run(ZgbjApplication.class, args);
-            System.out.println("Server startup done.");
+            log.info("Server startup done.");
         } catch (Exception e) {
-            System.out.println("服务xxx-support启动报错\n" + e);
+            log.error("服务xxx-support启动报错", e);
         }
     }
 
